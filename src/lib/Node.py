88637c18f424fb500e206e08c4a5d7eb34a7bbdc
Tristan Simpson <heytristaann@gmail.com>
2024-02-29 19:52:28 -0500

    feat: store and read graph data from a json file

--- src/lib/Node.py
+++ src/lib/Node.py
@@ -1,21 +1,76 @@
 ##
 ## Imports
 ##
-from dataclasses import dataclass
+import json
 
 
 ##
 ## Node class
 ##
-## If we need to add functions under the class, remove the @dataclass decorator
+## This class represents a node in a graph.
 ##
-@dataclass
 class Node:
-    id: int
-    label: str
-    group: int
-    title: str
+    ##
+    ## Constructor
+    ##
+    def __init__(self, index: int, x: int, y: int):
+        self.index = index
+        self.x = x
+        self.y = y
 
+        ##
+        ## End of function
+        ##
+
+    ##
+    ## String representation of the node
+    ##
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+        ##
+        ## End of function
+        ##
+
+    ##
+    ## Convert the node to a map
+    ##
+    def to_map(self) -> dict:
+        return {
+            "index": self.index,
+            "x": self.x,
+            "y": self.y,
+        }
+
+        ##
+        ## End of function
+        ##
+
+    ##
+    ## Convert the node to a json map
+    ##
+    def to_json(self) -> str:
+        return json.dumps(self.to_map())
+
+        ##
+        ## End of function
+        ##
+
+    ##
+    ## End of class
+    ##
+
+
+##
+## This tests the node class only if we're executing THIS current file.
+##
+## This is so that if we import the Node class from another file, this
+## code (in the 'if' statement) won't run.
+##
+if __name__ == "__main__":
+    node = Node(0, 1, 2)
+    print(node)
+    print(node.to_json())
 
 ##
 ## End of file
